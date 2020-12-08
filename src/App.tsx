f75c167402369d2524316c0d7d294e3290649e17
Dennis Passway <dennispassway@gmail.com>
2020-12-08 18:19:31 +0100

    Layout setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import client from 'part:@sanity/base/client'
 
-type Props = {
-  tool?: string
-}
+export const App = () => {
+  const [assets, setAssets] = useState([])
 
-export const App = ({ tool }: Props) => {
-  console.log(tool)
+  useEffect(() => {
+    (async function asyncFunction () {
+      const newAssets = await client.fetch(`*[_type == "sanity.imageAsset"]`, {})
+      setAssets(newAssets)
+      console.log(newAssets)
+    })()
+  }, [])
 
-  return <div>Hier is mijn App</div>
+  return (
+    <div>
+      <div>
+        <div>
+          <div><input type="search" placeholder="Search..." /></div>
+          <div>
+            <h2>Categories</h2>
+            <ul>
+              {['pdf', 'image', 'video'].map(x => <li><button>{x}</button></li>)}
+            </ul>
+          </div>
+          <div>
+            <h2>Tags</h2>
+            <ul>
+              {['projects', 'people', 'red'].map(x => <li><button>{x}</button></li>)}
+            </ul>
+          </div>
+          <div><button>Upload</button></div>
+        </div>
+        <div>
+          {assets.map(({ url}) => <img src={`${url}?w=200&h=200`} />)}
+        </div>
+      </div>
+
+      <div>
+        <button>Cancel</button> of <button>Insert</button>
+      </div>
+  </div>
+  )
 }
