8328cd24e6ee2bc9bf26c0bd23f71a245eaa3d03
Dennis Passway <dennispassway@gmail.com>
2020-12-10 12:12:53 +0100

    Expand asset type

--- src/types/Asset.ts
+++ src/types/Asset.ts
@@ -2,8 +2,11 @@ export interface Asset {
   _createdAt: string;
   _id: string;
   alt?: string;
+  extension: string;
   mimeType: string;
   originalFilename: string;
+  size: number;
   tags?: Array<string>;
+  title?: string;
   url: string;
 }
