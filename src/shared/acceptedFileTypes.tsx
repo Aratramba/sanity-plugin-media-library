e26609dedb2ba5279d5d9324cdf099ff1d5effe0
Dennis Passway <dennispassway@gmail.com>
2020-12-22 10:45:55 +0100

    Expand accepted file types

--- src/shared/acceptedFileTypes.tsx
+++ src/shared/acceptedFileTypes.tsx
@@ -1,10 +1,23 @@
 /* @TODO make this a plugin setting */
 
 export const acceptedFileTypes = [
+  'audio/*',
+  'image/*',
+  'video/*',
+  'application/json',
+  'application/ld+json',
   'application/msword',
   'application/pdf',
+  'application/rtf',
   'application/vnd.ms-excel',
   'application/vnd.ms-powerpoint',
-  'image/*',
+  'application/vnd.oasis.opendocument.presentation',
+  'application/vnd.oasis.opendocument.spreadsheet',
+  'application/vnd.oasis.opendocument.text',
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+  'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  'application/xml',
+  'application/zip',
   'text/plain',
+  'text/xml',
 ];
