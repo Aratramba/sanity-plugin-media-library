836cdca3c7b7dc560055dcbe09196be3e0d039ce
Aratramba <aratramba@gmail.com>
2021-02-08 15:07:58 +0100

    add custom viewport

--- jest-playwright.config.js
+++ jest-playwright.config.js
@@ -3,8 +3,21 @@ module.exports = {
   browsers: ['chromium'],
   exitOnPageError: false,
   launchOptions: {
-    headless: true,
+    headless: false,
   },
+  devices: [
+    {
+      name: 'test',
+      viewport: {
+        width: 1400,
+        height: 1400,
+      },
+      userAgent: 'chromium',
+      deviceScaleFactor: 1,
+      isMobile: false,
+      hasTouch: false,
+    },
+  ],
   serverOptions: {
     launchTimeout: 30000,
     command: 'yarn test-studio',
