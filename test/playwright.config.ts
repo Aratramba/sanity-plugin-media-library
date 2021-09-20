058e33c05bea184e775ddf8293443e52a6c6b9ff
Aratramba <aratramba@gmail.com>
2021-09-20 20:12:22 +0200

    Update playwright.config.ts

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -2,5 +2,6 @@ import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   globalSetup: require.resolve('./playwright.globalSetup'),
+  workers: process.env.GITHUB ? 1 : undefined,
 };
 export default config;
